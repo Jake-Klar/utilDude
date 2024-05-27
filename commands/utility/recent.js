946d1fcb1fbd6212ce19bad1bfd20e25afa848bb
imferolla <jakeklar17@gmail.com>
2024-05-27 11:01:38 +0100

    Recent Best Completed

--- commands/utility/recent.js
+++ commands/utility/recent.js
@@ -3,11 +3,6 @@ const { osuSecret } = require('../../config.json');
 const grant_type = "client_credentials";
 const scope = "public";
 const {SlashCommandBuilder} = require('discord.js');
-let body = `client_id=${osuId}&client_secret=${osuSecret}&grant_type=${grant_type}&scope=${scope}`
-const headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/x-www-form-urlencoded",
-}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,15 +13,45 @@ module.exports = {
                 .setRequired(true)
                 .setDescription('the player')
         ),
-    execute(interaction) {
-        fetch("https://osu.ppy.sh/oauth/token", {
-            method: "POST",
-            headers,
-            body: body,
+    async execute(interaction) {
+            //Generating an access token
+            const tokenResponse = await fetch("https://osu.ppy.sh/oauth/token", {
+            method: "POST",            
+            headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+            },         
+            body: `client_id=${osuId}&client_secret=${osuSecret}&grant_type=${grant_type}&scope=${scope}`,
         })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
-        interaction.reply('yer');
+        const tokenJson = await tokenResponse.json();
+        const accessToken = tokenJson.access_token;
+        
+        //Retrieving users osu ID
+        const player = interaction.options.getString('player');
+        const userResponse = await fetch(`https://osu.ppy.sh/api/v2/users/${player}/osu?key=username`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            },
+        })
+        const userJson = await userResponse.json();
+        const userId = userJson.id;
+
+        //Retrieving the users most recent score
+        const recentResponse = await fetch(`https://osu.ppy.sh/api/v2/users/${userId}/scores/recent?limit=1`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": `Bearer ${accessToken}`
+            },
+        })
+        const recentJson = await recentResponse.json();
+        console.log(recentJson);
+        interaction.reply(
+        `Time Set: ${recentJson[0].created_at}
+        \nPerformance Points: ${recentJson[0].pp}
+        \nMap: ${recentJson[0].beatmap.url}`);
     }
     
 }
